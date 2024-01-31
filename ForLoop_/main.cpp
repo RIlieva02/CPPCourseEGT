a48ce06a377b559011f5f7197364d76356b0239d
Ralitsa <157975611+RIlieva02@users.noreply.github.com>
2024-01-31 19:25:33 +0200

    Add files via upload

---
+++ ForLoop_/main.cpp
@@ -0,0 +1,54 @@
+//
+//  main.cpp
+//  sorce
+//
+//  Created by Ralitsa Toneva on 30.11.23.
+//
+
+#include <iostream>
+
+int main() {
+  
+    // Логически оператор &&
+//
+//    int number;
+//
+//    std::cout << "Whire your number: " << std::endl;
+//    std::cin >> number;
+//
+//    if (number > 0){
+//        if (number % 5 == 0 && number % 7 == 0){
+//            std::cout << "Number " <<  number << " is devisible!"<< std::endl;
+//        }
+//        else {
+//            std::cout << "Number " << number << " is not devisble!" << std::endl;
+//        }
+//    }
+//    else {
+//        std::cout << "The number is negative!" << std::endl;
+//    }
+//
+    // for цикъл
+//
+//    for (int i = 0; i <= 10; i++){
+//        std::cout << i << ' ';
+//    }
+//    std::cout << std::endl;
+    
+    // Изкарайте всички числа 0 - 1000, които се делят на 5 и на 7
+//    
+//    int sum = 0;
+//    
+//    for (int i = 0; i <= 1000; i++){
+//        if (i % 5 == 0 && i % 7 == 0){
+//            if (i == 280){
+//                continue;
+//            }
+//            std::cout << i << ' ';
+//            sum += i;
+//        }
+//    }
+//    std::cout << std::endl << "Sum = " << sum << std::endl;
+    
+    return 0;
+}
