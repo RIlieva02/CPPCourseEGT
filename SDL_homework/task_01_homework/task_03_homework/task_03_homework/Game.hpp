7ba568ac51cf0913b53df51829a3c0777a4921b5
Ralitsa <157975611+RIlieva02@users.noreply.github.com>
2024-02-14 18:42:26 +0200

    Add files via upload

---
+++ SDL_homework/task_01_homework/task_03_homework/task_03_homework/Game.hpp
@@ -0,0 +1,30 @@
+//
+//  Game.hpp
+//  task_03_homework
+//
+//  Created by Ralitsa Toneva on 14.02.24.
+//
+
+#ifndef Game_hpp
+#define Game_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include "Shape.hpp"
+
+class Game{
+public:
+    Game();
+    ~Game();
+    bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+    void render();
+    void handleEvents();
+    void clean();
+    bool isRunning();
+private:
+    SDL_Window* window = NULL;
+    SDL_Renderer* renderer = NULL;
+    bool running;
+    int currentFrame;
+};
+#endif /* Game_hpp */
---
+++ SDL_homework/task_01_homework/task_03_homework/task_03_homework/Shape.hpp
@@ -0,0 +1,18 @@
+//
+//  Shape.hpp
+//  task_03_homework
+//
+//  Created by Ralitsa Toneva on 14.02.24.
+//
+
+#ifndef Shape_hpp
+#define Shape_hpp
+
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+class Shape{
+public:
+    static void drawHexagon(SDL_Renderer* renderer, int centerX, int centerY, int radius);
+};
+#endif /* Shape_hpp */
