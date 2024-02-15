7af5e7d54f81c4744808bb6342aa5fa75817baf0
Ralitsa <157975611+RIlieva02@users.noreply.github.com>
2024-02-15 13:37:49 +0200

    Add files via upload

---
+++ EmployeeManagmentSystem_homework/Condition.h
@@ -0,0 +1,32 @@
+//
+//  Condition.h
+//  Conditions
+//
+//  Created by Ralitsa Toneva on 15.02.24.
+//
+
+// Implementing an Employee Management System
+//Note: Including clear and detailed comments is mandatory!
+//Task Description:
+//Develop a C++ program that models a simple employee management system. This program should include classes for employees and departments, showcasing the use of obiects, methods, and pointers.
+//Requirements:
+//Employee Class:
+//Attributes: name, employeeID, position.
+//Methods:
+// A constructor to initialize employee details.
+// display() to output employee details to the console.
+//Department Class:
+//Attributes: departmentName (string), employees (pointer to an array of Employee objects(), numEmployees (int to keep track of the number of employees).
+//Methods:
+//A constructor to initialize the department name.
+//addEmployee (Employee *employee) to add an employee to the department.
+// displayEmployees () to list all employees in the department.
+//Main Function:
+//Create several Employee objects and a Department object.
+//Add employees to the department and display the list of all employees in
+//Task Instructions:
+//Implement the Employee and Department classes as described.
+//In the main function, instantiate several Employee objects.
+//Create a Department object and use the addEmployee method to add employees to the department.
+// Use the displayEmployees method to list all employees in the department.
+// Ensure proper use of pointers, especially in managing the array of employees in the department.
