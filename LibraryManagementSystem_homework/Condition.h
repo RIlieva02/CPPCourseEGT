4f8cf75d60c4d6579f0f62f0b232688b397abff4
Ralitsa <157975611+RIlieva02@users.noreply.github.com>
2024-02-15 13:33:44 +0200

    Add files via upload

---
+++ LibraryManagementSystem_homework/Condition.h
@@ -0,0 +1,32 @@
+//
+//  Conditions.h
+//  Conditions
+//
+//  Created by Ralitsa Toneva on 15.02.24.
+//
+
+// Implementing a Library Management System
+//Note: Including clear and detailed comments is mandatory!
+//Task Description:
+//Create a C++ program for a simple library management system.
+//The program should include classes for books and library members, and demonstrate the use of objects, methods, and pointers.
+//Requirements:
+//Book Class:
+//Attributes: title (string), author (string), ISBN (string).
+// Methods:
+// A constructor that initializes the book details. display () to output the book details to the console.
+//Member Class:
+// Attributes: name (string), age (int), member ID (string).
+//Methods:
+//A constructor to initialize member details.
+//display () to output member details.
+//borrowBook (Book *book) to simulate borrowing a book, which takes a pointer to a Book object.
+//Main Function:
+//Create an array of Book objects and an array of Member objects.
+//Demonstrate the borrowing process by having a member borrow a book.
+//Use pointers to refer to and manipulate the book and member objects.
+//Task Instructions:
+//Implement the classes as described above.
+// In the main function, create at least two Book objects and two Member objects
+//Demonstrate the functionality of borrowBook () method by having members borrow books.
+// Make sure to use pointers appropriately when implementing the borrowBook method and handling objects in main.
