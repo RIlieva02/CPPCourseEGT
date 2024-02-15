5c40dfd16cd2be0494dadb3b2abdbcc6ed92fabf
Ralitsa <157975611+RIlieva02@users.noreply.github.com>
2024-02-15 13:53:12 +0200

    Add files via upload

---
+++ DrinkShop_homework/Condition.h
@@ -0,0 +1,27 @@
+//
+//  Condition.h
+//  Conditions
+//
+//  Created by Ralitsa Toneva on 15.02.24.
+//
+
+// Online shop:
+//*Drinks
+//Alcoholic; NoAlcoholic
+//Drinks
+//Name. ; Price. ; Quantity
+//Type (wine, gin, whiskey, water, juice, ice tea, -)
+//Print()
+//-NoAlcoholic
+//Liters ; CityTax
+//CalculatePrice() > Quantity * Price + CityTax
+//-Alcoholic
+//Volume ; Тах (акциз)
+//CalculatePrice() > Quantity * (Price * Tax)
+//Потребител - username, списк/вектор от порьчки
+//Порьчка: - напитки, дата, крайна сума
+//Метод за извеждане на инфо колко напитки са продадени от всеки вид
+//Метод който врща най-добрият клиент/клиенти (с най-скьла порьчка)
+//Метод който врща всички клиенти с порьчки съдржащи напитка от даден тип
+//Метод който врыща 10-те клиенти с порьчки сьдьржащи напитка от даден тип
+//с оборот на порьчката им над дадена сума */
