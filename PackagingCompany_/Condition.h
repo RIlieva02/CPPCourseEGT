07ebe446c416fd0307c50ae6b8c2930789bcd0bf
Ralitsa <157975611+RIlieva02@users.noreply.github.com>
2024-02-15 13:51:41 +0200

    Add files via upload

---
+++ PackagingCompany_/Condition.h
@@ -0,0 +1,24 @@
+//
+//  Condition.h
+//  Conditions
+//
+//  Created by Ralitsa Toneva on 15.02.24.
+//
+
+// Packages:
+//*Use the Package inheritance hierarchy created in Lecture
+//“Inheritance” to create a program that displays the address
+//information and calculates the shipping costs for several
+//Packages.
+//The program should contain a vector of Package pointers to
+//objects of classes TwoDayPackage and OvernightPackage.
+//Loop through the vector to process the Packages
+//polymorphically.
+//For each Package, invoke get functions to obtain the
+//address information of the sender and the recipient, then
+//print the two addresses as they would appear on mailing
+//labels.
+//Also, call each Package's calculateCost member function
+//and print the result.
+//Keep track of the total shipping cost for all Packages in the
+//vector, and display this total when the loop terminates.*/
