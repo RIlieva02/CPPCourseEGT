90fd197c846dce3dd9c06212f49eeabc0845435e
Ralitsa <157975611+RIlieva02@users.noreply.github.com>
2024-02-15 13:46:54 +0200

    Add files via upload

---
+++ School_homework/Condition.h
@@ -0,0 +1,14 @@
+//
+//  Condition.h
+//  Conditions
+//
+//  Created by Ralitsa Toneva on 15.02.24.
+//
+
+// We are given a school. In the school there are classes of students. Each class has
+//a set of teachers. Each teacher teaches a set of disciplines. Students have name
+//and unique class number. Classes have unique text identifier. Teachers have name
+//and title. Disciplines have name, number of lectures and number of exercises.
+// Define C++ classes for the school (School, Class, Student, Teacher, Discipline).
+//Keep the member fields private. Add constructors and accessor methods. Write a
+//testing class to construct and print a sample school.
